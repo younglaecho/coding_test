185a5bda4878065d75662aae670d52786498d30a
younglaecho <tks7205@gmail.com>
2021-10-12 04:25:35 +0900

    baekjoon 2193

--- backjoon/silver,bronze/2193.py
+++ backjoon/silver,bronze/2193.py
@@ -1,21 +1,8 @@
-binaries = []
-
-def generate(n, arr, i):
-  global binaries
-  if n == i:
-    arr= map(str, arr)
-    string = ''.join(arr)
-    binaries.append(string)
-    return
-
-  else:
-    arr[i]=0
-    generate(n, arr, i+1)
-    arr[i]=1
-    generate(n, arr, i+1)
-
 n = int(input())
-arr = [None]*n
-generate(n, arr, 0)
-for i in binaries:
-  print(i)
+
+dp = [1,1]*45
+
+for i in range(2, len(dp)):
+  dp[i] = dp[i-2]+dp[i-1]
+
+print(dp[n-1])
