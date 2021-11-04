a6426fa51d710be9fbd084da7235af6709d8090d
younglaecho <tks7205@gmail.com>
2021-11-04 23:51:51 +0900

    baekjoon 2751

--- backjoon/gold/4195.py
+++ backjoon/gold/4195.py
@@ -1,5 +1,8 @@
 c = int(input())
 
+network = {}
 for _ in range(c):
   name1, name2 = input().split()
-  print(name1, name2)
+  
+  network[name1]=name2
+  network[name2]=name1
---
+++ backjoon/silver,bronze/2751.py
@@ -0,0 +1,6 @@
+List = [int(input()) for _ in range(int(input()))]
+
+List.sort()
+
+for ele in List:
+  print(ele)
