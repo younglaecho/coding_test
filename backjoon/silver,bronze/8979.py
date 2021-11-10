2d9c387fc4c51e8b1d960865af89adc75126f883
younglaecho <tks7205@gmail.com>
2021-11-11 00:28:41 +0900

    baekjoon 8979

--- backjoon/silver,bronze/8979.py
+++ backjoon/silver,bronze/8979.py
@@ -4,8 +4,11 @@ for _ in range(n):
   i, gold, silver, bronze = map(int, input().split())
   Dict[i] ={'gold':gold, 'silver':silver, 'bronze': bronze}
 
+# print(Dict)
 cnt = 0 
 for i in Dict:
+  print(Dict[i]['gold'],  Dict[k]['gold'])
+
   if Dict[i]['gold'] > Dict[k]['gold']:
     cnt += 1
   elif Dict[i]['gold'] == Dict[k]['gold'] and Dict[i]['silver'] > Dict[k]['silver']:
@@ -13,4 +16,4 @@ for i in Dict:
   elif Dict[i]['gold'] == Dict[k]['gold'] and Dict[i]['silver'] == Dict[k]['silver'] and Dict[i]['bronze'] > Dict[k]['bronze']:
     cnt += 1
 
-print(cnt-1)
+print(cnt+1)
