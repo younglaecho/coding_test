5037533daf702b18027019259b44d5b1c1359e6e
younglaecho <tks7205@gmail.com>
2021-11-15 19:35:03 +0900

    baekjoon 1629

---
+++ backjoon/silver,bronze/1629.py
@@ -0,0 +1,14 @@
+def power(a, b, c) :
+  if b==1:
+    return a % c
+  else:
+    temp = power(a, b//2,c)
+    if b%2 ==1:
+      return (temp*temp*a) %c
+    else:
+      return (temp*temp) % c
+
+
+
+a, b, c = map(int, input().split())
+print(power(a,b,c))
