31e757db25814731040dbb8316723c88744266f5
younglaecho <tks7205@gmail.com>
2021-11-25 20:32:51 +0900

    baekjoon 1874, 1966

--- backjoon/silver,bronze/1966.py
+++ backjoon/silver,bronze/1966.py
@@ -1,20 +1,16 @@
 for _ in range(int(input())):
-  n, m = list(map(int, input().split()))
+  n, m = map(int, input().split())
   queue = list(map(int, input().split()))
   orders = [i for i in range(n)]
   cnt = 0
   while len(queue):
     if queue[0] == max(queue):
+      cnt += 1
       check = queue.pop(0)
       order = orders.pop(0)
-      cnt += 1
       if order == m:
         print(cnt)
         break
     else:
-      temp = queue[0]
-      queue.pop(0)
-      queue.append(temp)
-      temp = orders[0]
-      orders.pop(0)
-      orders.append(temp)
+      queue.append(queue.pop(0))
+      orders.append(orders.pop(0))
