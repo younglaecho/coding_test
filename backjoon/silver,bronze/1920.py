860fd06c9640e975322cf0b4a1f1bd8a33c4e723
younglaecho <tks7205@gmail.com>
2021-11-25 20:57:31 +0900

    baekjoon 1920

--- backjoon/silver,bronze/1920.py
+++ backjoon/silver,bronze/1920.py
@@ -1,18 +1,13 @@
 n = int(input())
 
-n_arr = list(map(int, input().split()))
+n_arr = set(map(int, input().split()))
 
 m = int(input())
 
 m_arr = list(map(int, input().split()))
 
-set = set()
-
-for i in n_arr:
-  set.add(i)
-
 for i in m_arr:
-  if i in set:
+  if i in n_arr:
     print(1)
   else:
     print(0)
