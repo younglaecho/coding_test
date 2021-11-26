d0d90692696ce221bdaa3e50e8af59d88fecc4ca
younglaecho <tks7205@gmail.com>
2021-11-26 20:32:15 +0900

    union_find: 1717

---
+++ backjoon/gold/1717.py
@@ -0,0 +1,27 @@
+def find(x):
+  if x == parent[x]:
+    return x
+  else:
+    p = find(parent[x])
+    parent[x] = p
+    return parent[x]
+
+def union(x,y):
+  x = find(x)
+  y = find(y)
+
+  parent[y] = x
+
+n,m = map(int,input().split())
+
+parent = [i for i in range(n+1)]
+
+for _ in range(m):
+  command, x, y = map(int, input().split())
+  if command==0:
+    union(x,y)
+  elif command==1:
+    if find(x)==find(y):
+      print('YES')
+    else:
+      print('NO')
