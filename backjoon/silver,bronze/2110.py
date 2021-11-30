9e5fa595089f158ee93955a27363a65a648d5b57
younglaecho <tks7205@gmail.com>
2021-11-30 16:24:04 +0900

    2110

---
+++ backjoon/silver,bronze/2110.py
@@ -0,0 +1,25 @@
+n, c = list(map(int, input().split()))
+
+array = []
+for _ in range(n):
+  array.append(int(input()))
+array.sort()
+
+start = array[1]-array[0]
+end = array[-1]-array[0]
+result = 0
+while (start<=end):
+  cnt = 1
+  gap = (end+start)//2
+  value = array[0]
+  for i in range(1, len(array)):
+    if array[i]>=value+gap:
+      cnt+=1
+      value= array[i]
+  if cnt >= c:
+    start = gap+1
+    result = gap
+  else:
+    end = gap-1
+
+print(result)
