aa07ff55de56a0a40816b657de72a768a3104f1b
younglaecho <tks7205@gmail.com>
2021-12-11 23:59:19 +0900

    greedy 1080

---
+++ backjoon/silver,bronze/greedy/1080.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+
+prev_matrix = [list(map(int, input())) for _ in range(n)]
+result_matrix = [list(map(int, input())) for _ in range(n)]
+
+count = 0
+for i in range(n-2):
+  for j in range(m-2):
+    if prev_matrix[i][j] != result_matrix[i][j]:
+      for a in range(3):
+        for b in range(3):
+          prev_matrix[i+a][j+b] = 1-prev_matrix[i+a][j+b]
+      count+=1
+
+breaked = False
+for i in range(n):
+  for j in range(m):
+    if prev_matrix[i][j] != result_matrix[i][j]:
+      breaked = True
+      break
+if n<3 or m<3:
+  breaked = True
+if breaked:
+  print(-1)
+else:
+  print(count)
