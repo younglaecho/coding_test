082cc3a5a7b7d0411ddb80632bbc37b433f25a5d
younglaecho <tks7205@gmail.com>
2021-12-16 23:06:01 +0900

    stack 17298 add wrong solution

--- backjoon/gold/stack/17298.py
+++ backjoon/gold/stack/17298.py
@@ -1,14 +1,28 @@
+# n = int(input())
+# A = list(map(int, input().split()))
+# answer = [-1] * n
+# stack = []
+
+
+# stack.append(0)
+# for i in range(1, n):
+#     while stack and A[stack[-1]] < A[i]:
+#         answer[stack.pop()] = A[i]
+#     stack.append(i)
+
+
+# print(*answer)
+
 n = int(input())
 A = list(map(int, input().split()))
-answer = [-1] * n
-stack = []
+result = []
+for i in range(0, n):
+  for j in range(i+1,n):
+    if A[j]>A[i]:
+      result.append(A[j])
+      break
+  else:
+    result.append(-1)
 
-
-stack.append(0)
-for i in range(1, n):
-    while stack and A[stack[-1]] < A[i]:
-        answer[stack.pop()] = A[i]
-    stack.append(i)
-
-
-print(*answer)
+for ele in result:
+  print(ele, end=" ")
