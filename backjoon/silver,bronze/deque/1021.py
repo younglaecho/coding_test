2391c6e7c8917086a9e7f7ad6d4d9e088b762c57
younglaecho <tks7205@gmail.com>
2022-01-03 23:26:16 +0900

    deque 1138

---
+++ backjoon/silver,bronze/deque/1021.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+arr = [i for i in range(1, n+1)]
+elements = list(map(int, input().split()))
+result = []
+while elements:
+  element = elements.pop(0)
+  print(arr, element)
+  right = arr[arr.index(element):]
+  left = arr[:arr.index(element)]
+  print(right)
+  print(left)
+  if len(right)> len(left) :
+    arr = right + left
+    result.append(len(left))
+    arr.pop(0)
+  else:
+    arr = right + left
+    result.append(len(right))
+    arr.pop(0)
+
+
+print(sum(result))
