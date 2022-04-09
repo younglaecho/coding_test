c8ae054c948882cc7b74cacd8ae6e1eee6a7be7c
younglaecho <tks7205@gmail.com>
2022-04-09 23:49:13 +0900

    cho

---
+++ baekjoon/silver,bronze/math/1002.py
@@ -0,0 +1,26 @@
+import math
+
+a = int(input())
+for i in range(a):
+    x1, y1, r1, x2, y2, r2 = map(float, input().split())
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    if r1 < r2:
+        r1, r2 = r2, r1
+    if distance == 0:
+        if r1 == r2:
+            print(-1)
+        else:
+            print(0)
+    elif r1 > distance:
+        if r1 - r2 > distance:
+            print(0)
+        elif r1 - r2 == distance:
+            print(1)
+        else:
+            print(2)
+    elif distance > r1 + r2:
+        print(0)
+    elif distance == r1 + r2:
+        print(1)
+    else:
+        print(2)
